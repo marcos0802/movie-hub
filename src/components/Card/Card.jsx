eae0898001a9e82431675c070222a7b5c6d68556
root <root@localhost.localdomain>
2021-05-11 16:31:28 +0100

    style: Set new display and style for card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,64 @@
 import React from "react";
 import HowToVoteIcon from "@material-ui/icons/HowToVote";
 import { img_300, unavailable } from "../../config/config";
-import "./Style.css";
 import ModalContainer from "../Modal/Modal";
-import { CardImage, CardTitle, CardFooter } from "./CardStyle";
+import { CardTitle, CardFooter } from "./CardStyle";
+import "./Style.css";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  height: 290px;
+  box-shadow: 0px 1px 5px black;
+  border-radius: 6px;
+  border: 2px solid steelblue;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  &:hover {
+    transform: scale(1.05);
+    .info-hidden {
+      transition: all 2s ease;
+      display: block;
+      text-align: center;
+      opacity: 1;
+      background: #000;
+    }
+  }
+  .info-hidden {
+    display: none;
+    opacity: 0;
+    z-index: 999;
+  }
+`;
 
 const Card = ({ id, poster, title, date, media_type, vote_average }) => {
   return (
     <ModalContainer media_type={media_type} id={id}>
-      <CardImage
-        src={poster ? `${img_300}${poster}` : unavailable}
-        alt={title}
-      />
-      <CardTitle>{title}</CardTitle>
-      <CardFooter>
-        <span>{media_type === "tv" ? "TV Series" : "Movie"}</span>
-        <span>{date}</span>
-        <span className="vote">
-          <HowToVoteIcon style={{ fontSize: "13px" }} /> {vote_average}
-        </span>
-      </CardFooter>
+      <div style={{ padding: "10px" }}>
+        <CardContainer
+          style={{
+            background: `url(${
+              poster ? `${img_300}${poster}` : unavailable
+            }) no-repeat`,
+            backgroundSize: "cover",
+          }}
+        >
+          {/* <CardImage
+          src={poster ? `${img_300}${poster}` : unavailable}
+          alt={title}
+        /> */}
+          <div className="info-hidden">
+            <CardTitle>{title}</CardTitle>
+            <CardFooter>
+              <span>{media_type === "tv" ? "TV Series" : "Movie"}</span>
+              <span>{date}</span>
+              <span className="vote">
+                <HowToVoteIcon style={{ fontSize: "13px" }} /> {vote_average}
+              </span>
+            </CardFooter>
+          </div>
+        </CardContainer>
+      </div>
     </ModalContainer>
   );
 };
--- src/components/Card/CardStyle.jsx
+++ src/components/Card/CardStyle.jsx
@@ -1,8 +1,55 @@
 import styled from "styled-components";
 
+export const CardContainer = styled.div`
+  .media {
+    width: calc(20% - 20px);
+    margin-bottom: 10px;
+    margin-left: 20px;
+    box-shadow: 0px 1px 3px black;
+    border-radius: 6px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    position: relative;
+    padding: 10px;
+  }
+
+  .media:hover {
+    background: #111;
+    transform: scale(1.05);
+  }
+  .info-hidden {
+    display: none;
+    opacity: 0;
+  }
+
+  @media (max-width: 976px) {
+    .media {
+      width: calc(25% - 10px);
+    }
+  }
+
+  @media (max-width: 747px) {
+    .media {
+      width: calc(33% - 10px);
+    }
+  }
+
+  @media (max-width: 600px) {
+    .media {
+      width: calc(50% - 10px);
+    }
+  }
+
+  @media (max-width: 390px) {
+    .media {
+      width: calc(80% - 10px);
+    }
+  }
+`;
+
 export const CardImage = styled.img`
   width: 100%;
-  height: 180px;
+  height: 100%;
   object-fit: cover;
   border-top-left-radius: 6px;
   border-top-right-radius: 6px;
